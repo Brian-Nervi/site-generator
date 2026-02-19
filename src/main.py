d098a9b78c0c5108b6b4ad42e42d0e68677b10a8
WLHamu <hancraft321@gmail.com>
2026-02-19 22:29:15 +0000

    page hosted in github and removed /public for /doc

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import shutil
 import os
+import sys
 from blocks import markdown_to_html_node
 
-def clean_public():
-    shutil.rmtree("public", ignore_errors=True)
+def clean_docs():
+    shutil.rmtree("docs", ignore_errors=True)
 
 def copy_files_recursive(src, dst):
     if not os.path.exists(dst):
@@ -23,7 +24,7 @@ def extract_title(markdown):
             return line.lstrip()[2:].strip()
     raise Exception("Title not found in markdown")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath=None):
     print(f"Generating page from {from_path} to {dest_path} using template {template_path}")
     with open(from_path, "r") as file:
         markdown = file.read()
@@ -34,6 +35,9 @@ def generate_page(from_path, template_path, dest_path):
         html = html[len("<div>") : -len("</div>")]
     title = extract_title(markdown)
     page_content = template_content.replace("{{ Content }}", html).replace("{{ Title }}", title)
+    if basepath:
+        page_content = page_content.replace('href="/', f'href="{basepath}')
+        page_content = page_content.replace('src="/', f'src="{basepath}')
     final_dest_path = os.path.dirname(dest_path)
     if final_dest_path:
         if not os.path.exists(final_dest_path):
@@ -41,21 +45,24 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, "w") as f:
         f.write(page_content)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath=None):
     for file in os.listdir(dir_path_content):
         entry = os.path.join(dir_path_content, file) #makes it a full path
         if os.path.isdir(entry):
-            generate_pages_recursive(entry, template_path, os.path.join(dest_dir_path, file))
+            generate_pages_recursive(entry, template_path, os.path.join(dest_dir_path, file), basepath)
         if os.path.isfile(entry) and entry.endswith(".md"):
-            generate_page(entry, template_path, os.path.join(dest_dir_path, file[:-3] + ".html"))
+            generate_page(entry, template_path, os.path.join(dest_dir_path, file[:-3] + ".html"), basepath)
 
 
     
 
 def main():
-    clean_public()
-    copy_files_recursive("./static", "./public")
-    generate_pages_recursive("./content", "./template.html", "./public")
+    clean_docs()
+    copy_files_recursive("./static", "./docs")
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+    generate_pages_recursive("./content", "./template.html", "./docs", basepath)
+
+    
     pass
 
 main()
